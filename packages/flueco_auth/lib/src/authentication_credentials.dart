8791e7eae49f9f22790a676cd06988712485da26
Maxime F. <maxime.user@outlook.fr>
2025-05-18 13:36:03 +0200

    refactor(flueco_auth): enhance authentication credentials classes and update naming convention

--- packages/flueco_auth/lib/src/authentication_credentials.dart
+++ packages/flueco_auth/lib/src/authentication_credentials.dart
@@ -1,17 +1,38 @@
 import 'package:flueco_auth/src/exceptions/authentication_exception.dart';
 
 /// Base credentials
-abstract class Credentials {}
+abstract class Credentials {
+  /// Creates an instance of [Credentials]
+  const Credentials();
+}
 
-/// Credentials send to authenticate.
-abstract class AuthenticationCredentials extends Credentials {}
+/// Credentials sent for authentication.
+abstract class AuthenticationCredentials extends Credentials {
+  /// Creates an instance of [AuthenticationCredentials]
+  const AuthenticationCredentials();
+}
 
 /// Credentials used to refresh authentication
-abstract class RefreshCredentials extends Credentials {}
+abstract class RefreshCredentials extends Credentials {
+  /// Creates an instance of [RefreshCredentials]
+  const RefreshCredentials();
+}
 
 /// Exception thrown when invalid credentials are provided.
 class InvalidAuthenticationCredentialsException
-    extends AuthenticationException {}
+    extends AuthenticationException {
+  /// Creates an instance of [InvalidAuthenticationCredentialsException].
+  const InvalidAuthenticationCredentialsException({
+    super.cause,
+    super.stackTrace,
+  });
+}
 
 /// Exception thrown when invalid refresh credentials are provided.
-class InvalidRefreshCredentialsException extends AuthenticationException {}
+class InvalidRefreshCredentialsException extends AuthenticationException {
+  /// Creates an instance of [InvalidRefreshCredentialsException]].
+  const InvalidRefreshCredentialsException({
+    super.cause,
+    super.stackTrace,
+  });
+}
