8b313b9ab1f9e63f79270f89dc91d21658be3f55
Maxime F. <maxime.user@outlook.fr>
2025-05-25 18:43:42 +0200

    feat(flueco_state_management): implement StateNotifier, ViewModel, and ViewState classes

---
+++ packages/flueco_state_management/lib/src/state/view_state.dart
@@ -0,0 +1,7 @@
+import 'package:equatable/equatable.dart' show EquatableMixin;
+
+/// Base class of a view state
+abstract class ViewState with EquatableMixin {
+  /// Constructor
+  const ViewState();
+}
