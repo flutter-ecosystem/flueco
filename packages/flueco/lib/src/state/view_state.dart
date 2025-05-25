d158616a56dee6a3ed2fd35ac78aa332df9249d9
Maxime F. <maxime.user@outlook.fr>
2025-05-25 19:00:42 +0200

    refactor(flueco): remove unused state management classes
    BREAKING CHANGE: State management classes like ViewModel, ViewState or StateNotifier are moved in a separate package flueco_state_management

--- packages/flueco/lib/src/state/view_state.dart
+++
@@ -1,7 +0,0 @@
-import 'package:equatable/equatable.dart' show EquatableMixin;
-
-/// Base class of a view state
-abstract class ViewState with EquatableMixin {
-  /// Constructor
-  const ViewState();
-}
