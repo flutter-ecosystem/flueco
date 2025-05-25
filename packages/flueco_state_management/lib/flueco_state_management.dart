5e7db99e4fb8580fd351f3a8635e32d381af5285
Maxime F. <maxime.user@outlook.fr>
2025-05-25 18:44:27 +0200

    feat(flueco_state_management): add exports for state management and helper classes

--- packages/flueco_state_management/lib/flueco_state_management.dart
+++ packages/flueco_state_management/lib/flueco_state_management.dart
@@ -1,3 +1,8 @@
 library;
 
 export 'src/state_data_provider.dart';
+export 'src/state/state_notifier.dart';
+export 'src/state/view_state.dart';
+export 'src/state/view_model.dart';
+export 'src/helpers/safe_zone_result.mixin.dart';
+export 'src/helpers/computation.dart';
