68adcadfcf4eac319ab355466c6a5db6d2b44243
Maxime F. <maxime.user@outlook.fr>
2025-05-25 18:54:37 +0200

    chore(flueco_state_management): add provider package exports for state management

--- packages/flueco_state_management/lib/flueco_state_management.dart
+++ packages/flueco_state_management/lib/flueco_state_management.dart
@@ -6,3 +6,6 @@ export 'src/state/view_state.dart';
 export 'src/state/view_model.dart';
 export 'src/helpers/safe_zone_result.mixin.dart';
 export 'src/helpers/computation.dart';
+
+export 'package:provider/provider.dart';
+export 'package:provider/single_child_widget.dart';
